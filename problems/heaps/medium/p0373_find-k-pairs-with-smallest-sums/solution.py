5f24bf54da9c6b98a793e1546d556ab8ad62155e
RackAttack86 <RackAttack86@users.noreply.github.com>
2026-01-21 00:39:16 -0500

    Solved find k pairs with smallest sums. Tests pass

--- problems/heaps/medium/p0373_find-k-pairs-with-smallest-sums/solution.py
+++ problems/heaps/medium/p0373_find-k-pairs-with-smallest-sums/solution.py
@@ -49,15 +49,21 @@ class Solution:
     """
 
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2:
+            return []
+
         heap = []
-        for i in range(k):
-            for j in range(k):
-                heapq.heappush(heap, (nums1[i]+nums2[j], [nums1[i],nums2[j]]))
+        for j in range(min(len(nums2), k)):
+            heapq.heappush(heap, (nums1[0] + nums2[j], 0, j))
 
         res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap))
-        
+        while heap and len(res) < k:
+            _, i, j = heapq.heappop(heap)
+            res.append([nums1[i], nums2[j]])
+
+            if i + 1 < len(nums1):
+                heapq.heappush(heap, (nums1[i+1] + nums2[j], i+1, j))
+
         return res
 
 # Metadata for tracking
